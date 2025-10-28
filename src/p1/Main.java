bce16be3af87d4a7caf8f1688adaa53490f73f3e
Jona Dress <jdress@smail.uni-koeln.de>
2025-10-28 15:55:19 +0100

    <allespraesenz>

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,22 +1,20 @@
 package p1;
+
 import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
-	Scanner s = new Scanner(System.in);
-	
-	String zeile = new String();
-	
-	zeile = s.nextLine();
-	
-	System.out.println(zeile);
-	
-	s.close();
-	
-	
-	
-	
-	
-    }
+
+		Scanner s = new Scanner(System.in);
+
+		String zeile = new String();
+
+		zeile = s.nextLine();
+
+		System.out.println(zeile);
+
+		s.close();
+
+	}
 
 }
---
+++ src/p3/P3_main.java
@@ -0,0 +1,23 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		int i=10;
+		int j=20;
+		int k=30;
+		
+		if (i<j) {
+			System.out.println("A");
+			
+	    }
+		if (i<k) {
+			System.out.println("B");
+		}
+			
+		if (j<k) {
+			System.out.println("C");
+		}
+	}
+
+}
