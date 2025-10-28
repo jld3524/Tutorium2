ea16c23313eab36312c8eddd3be08d802ea832c9
Jona Dress <jdress@smail.uni-koeln.de>
2025-10-28 15:30:46 +0100

    <Aufgabe 1 und 2 Präsenz>

---
+++ src/p1/Main.java
@@ -0,0 +1,22 @@
+package p1;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+	
+	String zeile = new String();
+	
+	zeile = s.nextLine();
+	
+	System.out.println(zeile);
+	
+	s.close();
+	
+	
+	
+	
+	
+    }
+
+}
