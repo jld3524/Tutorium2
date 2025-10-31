895eb7bf4ca6ca164079cd38fc418e2638e3f8d9
Jona Dress <jdress@smail.uni-koeln.de>
2025-10-31 12:55:16 +0100

    <fertig>

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,17 +3,15 @@ package h2;
 public class H2_main {
 
 	public static void main(String[] args) {
-		int i = 1;
-		int j = 2;
-		int k = 3;
-		int min;
-		int max;
-		
-		 min = Math.min(i, Math.min(j, k));  // gibt den kleineren Wert zurück
-		 //max = Math.max(i, j, k);  // gibt den größeren Wert zurück
+		int i = 15;
+        int j = 7;
+        int k = 23;
 
-	        System.out.println("Min: " + min);
-	        //System.out.println("Max: " + max);
-	}
+        int min = Math.min(i, Math.min(j, k));
+        int max = Math.max(i, Math.max(j, k));
 
+        
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
 }
--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,28 @@
 package h3;
 
 public class H3_main {
+    public static void main(String[] args) {
+        int i = 210;
+        int j = 102;
+        int k = -10;
 
+        boolean expr1 = i > j;
+        boolean expr2 = i > 200;
+        boolean expr3 = j > 100;
+
+        if (expr1 && !expr2 && !expr3) {
+            k = 1;
+        } else if (expr1 && expr2 && !expr3) {
+            k = 2;
+        } else if (expr1 && expr2 && expr3) {
+            k = 3;
+        } else if (!expr1 && !expr2 && !expr3) {
+            k = 4;
+        } else {
+            k = -10;
+        }
+
+        System.out.println("Werte:  k = " + k);
+    }
 }
+
